9c6f0826540d2db40f7e2bceb03b8a40d4379ff6
Teije <teijevhemert@gmail.com>
2023-02-24 23:04:29 +0100

    Add models for a SpotPrice and CurrencyPair to store values at specific times

---
+++ CurrencyPair.h
@@ -0,0 +1,48 @@
+#ifndef CURRENCYPAIR
+#define CURRENCYPAIR
+
+#include "Base.h"
+
+class CurrencyPair: public Base  {
+  private:
+    String currencyLabel;
+    String counterCurrencyLabel;
+    float counterValue;
+
+  public:
+    CurrencyPair(String currencyLabel, String counterCurrencyLabel, float counterValue) {
+      this->currencyLabel = currencyLabel;
+      this->counterCurrencyLabel = counterCurrencyLabel;
+      this->counterValue = counterValue;
+    }
+
+    const String toString() override {
+      return currencyLabel + "-" + counterCurrencyLabel + ": " + String(counterValue);
+    }
+
+    void SetCurrencyLabel(String currencyLabel) {
+      this->currencyLabel = currencyLabel;
+    }
+
+    String GetCurrencyLabel() {
+      return this->currencyLabel;
+    }
+
+    void SetCounterCurrencyLabel(String counterCurrencyLabel) {
+      this->counterCurrencyLabel = counterCurrencyLabel;
+    }
+
+    String GetCounterCurrencyLabel() {
+      return this->counterCurrencyLabel;
+    }
+
+    void SetCounterValue(float counterValue) {
+      this->counterValue = counterValue;
+    }
+
+    float GetCounterValue() {
+      return this->counterValue;
+    }
+};
+
+#endif
---
+++ SpotPrice.h
@@ -0,0 +1,39 @@
+#ifndef SPOTPRICE
+#define SPOTPRICE
+
+#include <Arduino.h>
+#include "CurrencyPair.h"
+
+class SpotPrice {
+  private:
+    CurrencyPair currency;
+    String timestamp;
+
+  public:
+    SpotPrice(CurrencyPair currency, String timestamp) {
+      this->currency = currency;
+      this->timestamp = timestamp;
+    }
+
+    String ToString() {
+      return currency.ToString() + ", Timestamp: " + timestamp;
+    }
+
+    void SetCurrency(CurrencyPair currency) {
+      this->currency = currency;
+    }
+
+    CurrencyPair GetCurrency() {
+      return this->currency;
+    }
+
+    void SetTimestamp(String timestamp) {
+      this->timestamp = timestamp;
+    }
+
+    String GetTimestamp() {
+      return this->timestamp;
+    }
+};
+
+#endif
