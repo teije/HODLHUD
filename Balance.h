2ed0ac7fda00d35d674752844a8e1a7da499c46c
Teije <teijevhemert@gmail.com>
2023-03-03 18:55:10 +0100

    Resolve ifndef-bug & cleanup

--- Balance.h
+++ Balance.h
@@ -1,22 +1,28 @@
-#ifndef APICALLER
-#define APICALLER
+#ifndef BALANCE
+#define BALANCE
 
-class Balance {
+#include "CurrencyPair.h"
+
+class Balance : public Base {
   public:
-    CurrencyPair currencyPair;
     float amount;
+    CurrencyPair currencyPair;
+    String Name;
 
-    Balance(CurrencyPair currencyPair, float amount) {
-      this->currencyPair = currencyPair;
-      this->amount = amount;
+    Balance(CurrencyPair currencyPair, float amount) : currencyPair(currencyPair), amount(amount) 
+    {
+      this->Name = currencyPair.Label();
+
+      println("Created " + Name + " " + Type());
     }
 
   private:
+
+  public:
     String Type() {
       return "Balance";
     }
-
-  public:
+    
     String ToString() {
       String output = "Type: " + Type() + "\n";
       output += "Currency Pair: " + currencyPair.currencyLabel + "/" + currencyPair.counterCurrencyLabel + "\n";
