ce6b698ad452cb9afca6444d18ce95d0a88b1388
Teije <teijevhemert@gmail.com>
2023-03-03 10:16:03 +0100

    Add method to parse human readable timestamp to unix timestamp

--- ESP32Clock.h
+++ ESP32Clock.h
@@ -57,6 +57,24 @@ class ESP32Clock : public Base {
       print(humanReadableTime);
       return humanReadableTime;
     }
+
+    uint32_t parseHumanReadableTime(String timestamp) {
+      if (!isClockConfigured()) {
+        configureClock();
+      }
+      struct tm timeinfo = {};
+      timeinfo.tm_year = atoi(timestamp.substring(0,4).c_str()) - 1900;
+      timeinfo.tm_mon = atoi(timestamp.substring(5,7).c_str()) - 1;
+      timeinfo.tm_mday = atoi(timestamp.substring(8,10).c_str());
+      timeinfo.tm_hour = atoi(timestamp.substring(11,13).c_str());
+      timeinfo.tm_min = atoi(timestamp.substring(14,16).c_str());
+      timeinfo.tm_sec = atoi(timestamp.substring(17,19).c_str());
+      time_t epoch_time = mktime(&timeinfo);
+      uint32_t unix_time = epoch_time;
+      println("Parsed Unix to Human-Readable Timestamp: ");
+      Serial.printf("%u", unix_time);
+      return unix_time;
+    }
     
   private:
     WiFiUDP ntpUDP;
